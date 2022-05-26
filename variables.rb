2cfd911a3071655f0408889e8b73a5199fab3269
caprigamma <caprigamma@gmail.com>
2022-05-26 12:43:21 -0400

    agregando ejemplo de variable en Ruby

---
+++ variables.rb
@@ -0,0 +1,14 @@
+#Sumando dos valores
+x = 10
+y = 20
+suma = x + y
+puts suma
+#multiplicando 2 variables"
+precio_dolar=20
+precio_mx= 770
+conversion= precio_dolar * precio_mx
+puts conversion
+#Variables tipo texto"
+texto_saludo="Hola, Desde México"
+puts texto_saludo
+puts "Hasta el próximo código"
