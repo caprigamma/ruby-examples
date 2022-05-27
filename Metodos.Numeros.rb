e6407b58e1a028ccddef8f01eb05a098e426b4c3
caprigamma <caprigamma@gmail.com>
2022-05-27 10:21:54 -0400

    Agregando ejemplo de metodos

---
+++ Metodos.Numeros.rb
@@ -0,0 +1,20 @@
+
+numero_entero=1777
+#metodo para numero entero siguiente o succesor
+puts numero_entero.next
+puts numero_siguiente=numero_entero.succ
+#numero para validar si un numero es par o impar
+puts numero_entero.odd?
+puts numero_entero.even?
+puts numero_siguiente.odd?
+puts numero_siguiente.even?
+     #conversion de entero a flotante
+     puts numero_siguiente/numero_entero.to_f
+     #Longitud de un numero
+     puts 800/270.to_f
+     #Ponerle en digitos un numero
+     print 360.digits
+     print 78905.digits
+     #redondear un numero flotante
+     puts 5609.78203.round
+     puts 5609.78203.round
--- numbers.rb
+++ numbers.rb
@@ -8,9 +8,11 @@ puts la_suma.class
 #Numeros de tipo flotante_decimales
   num_flotante1=2.13
   num_flotante2=7.67
-  otra_suma= num_flotante1 + num_flotante2
+  otra_suma= num_flotante1 + num_flotante2ruby
   puts num_flotante1.class
   puts num_flotante2.class
   puts otra_suma.class
+#suma de integrer y float
   suma_final= la_suma + otra_suma
   suma_final.class
+  puts suma_final.class
