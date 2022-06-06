6512a4319ba79a2137ec7e34327dbc8490e2cf14
caprigamma <caprigamma@gmail.com>
2022-06-06 12:57:54 -0400

    Agregando ejemplo velocidad

---
+++ emprendedor/example_argv.rb
@@ -0,0 +1,5 @@
+primero = ARGV[0].to_i
+segundo = ARGV[1].to_i
+tercero = ARGV[2].to_i
+suma= primero+segundo+tercero
+puts "La variable de ARGV #{primero} + #{segundo} + #{tercero} y la suma #{suma}"
