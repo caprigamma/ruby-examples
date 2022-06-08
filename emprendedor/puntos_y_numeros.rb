3ed3051a731c3d444cfd413d11de5cb36f3c3d89
caprigamma <caprigamma@gmail.com>
2022-06-08 10:20:25 -0400

    Agregando ejemplo  bloque each cuantos numeros quiere imprimir

---
+++ emprendedor/puntos_y_numeros.rb
@@ -0,0 +1,9 @@
+print "Cuantos numeros quiere imprimir: "
+n= gets.chomp.to_i
+#(1..n).each { |var_block | print "#{var_block".}
+
+#bloque usando each imprimir numeros con puntos
+(1..n).each  do |var_block|
+    print "#{var_block}."
+
+end
